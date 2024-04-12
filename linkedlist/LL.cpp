77e954adecf52909af0c9366515819e9c1f8b7cb
jyotirmoybanerjee2222 <jyotirmoybanerjee0511@gmail.com>
2024-04-12 21:26:18 +0530

    first commit

---
+++ linkedlist/LL.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1,Node* next1)
+    {
+       data=data1;
+       next=next1;
+    }
+    public:
+    Node(int data1)
+    {
+       data=data1;
+       next=nullptr;
+    }
+
+
+};
+
+int main(){
+    vector<int> arr={2,5,8,7};
+    Node* y= new Node{arr[0],nullptr};
+    cout<<y;
+    cout<<y->data<<" "<<y->next;
+
+    Node x=  Node{arr[0],nullptr};
+    cout<<x.data;
+    cout<<x.next;
+
+}
